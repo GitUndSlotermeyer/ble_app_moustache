9fe2b847cb44508d1c38e8e8226dd55d2baeeb9e
Hrvoje Elez <hrvoje.elez@globallogic.com>
2021-08-30 17:36:45 +0200

    Scanner added

---
+++ scanner/pca10056/s140/armgcc/_build/nrf52840_xxaa/nrf_pwr_mgmt.c.d
@@ -0,0 +1,286 @@
+_build/nrf52840_xxaa/nrf_pwr_mgmt.c.o: \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/pwr_mgmt/nrf_pwr_mgmt.c \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_common.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/string.h ../config/sdk_config.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/nordic_common.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_os.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_errors.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_error.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_util.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52840.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf52/nrf_mbr.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_svc.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_macros.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/nrf_assert.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/pwr_mgmt/nrf_pwr_mgmt.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_errors.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/experimental_section_vars/nrf_section_iter.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/nordic_common.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/nrf_assert.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_util.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/mutex/nrf_mtx.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/atomic/nrf_atomic.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/hal/nrf_power.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/nrfx.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/integration/nrfx/nrfx_config.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/drivers/nrfx_common.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf_peripherals.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52840_peripherals.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/integration/nrfx/nrfx_glue.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/integration/nrfx/legacy/apply_old_config.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/soc/nrfx_irqs.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/soc/nrfx_irqs_nrf52840.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_util_platform.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_soc.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_svc.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_error.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_error_soc.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_nvic.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_error.h \
+ /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/types.h /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/sys/_pthreadtypes.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_error_weak.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/soc/nrfx_coredep.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/soc/nrfx_atomic.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_resources.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_sd_def.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_soc.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/drivers/nrfx_errors.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_error.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_ctrl.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_types.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/src/nrf_log_ctrl_internal.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_backend_interface.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/memobj/nrf_memobj.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/balloc/nrf_balloc.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_instance.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/strerror/nrf_strerror.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/src/nrf_log_internal.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_types.h \
+ ../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/common/nrf_sdh.h
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_common.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+/usr/include/newlib/string.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/newlib.h:
+
+/usr/include/newlib/_newlib_version.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/include/newlib/sys/string.h:
+
+../config/sdk_config.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/nordic_common.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_os.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_errors.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_error.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_util.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52840.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_version.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_compiler.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/mpu_armv7.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/system_nrf52840.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52840_bitfields.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf52/nrf_mbr.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_svc.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_macros.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/nrf_assert.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/pwr_mgmt/nrf_pwr_mgmt.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_errors.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/experimental_section_vars/nrf_section_iter.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/experimental_section_vars/nrf_section.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/nordic_common.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/nrf_assert.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_util.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/mutex/nrf_mtx.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/atomic/nrf_atomic.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/hal/nrf_power.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/nrfx.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/integration/nrfx/nrfx_config.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/drivers/nrfx_common.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf_peripherals.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52840_peripherals.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/integration/nrfx/nrfx_glue.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/integration/nrfx/legacy/apply_old_config.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/soc/nrfx_irqs.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/soc/nrfx_irqs_nrf52840.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_util_platform.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_soc.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_svc.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_error.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_error_soc.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_nvic.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_error.h:
+
+/usr/include/newlib/stdio.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+/usr/include/newlib/sys/types.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+/usr/include/newlib/sys/_pthreadtypes.h:
+
+/usr/include/newlib/machine/types.h:
+
+/usr/include/newlib/sys/stdio.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_error_weak.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/soc/nrfx_coredep.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/soc/nrfx_atomic.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/sdk_resources.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_sd_def.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/s140/headers/nrf_soc.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/modules/nrfx/drivers/nrfx_errors.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_error.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_ctrl.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_types.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/src/nrf_log_ctrl_internal.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_backend_interface.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/memobj/nrf_memobj.h:
+
+/usr/include/newlib/stdlib.h:
+
+/usr/include/newlib/machine/stdlib.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/balloc/nrf_balloc.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_instance.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/experimental_section_vars/nrf_section.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/strerror/nrf_strerror.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/src/nrf_log_internal.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/libraries/log/nrf_log_types.h:
+
+../../../../../nRF5_SDK_17.0.2_d674dde/components/softdevice/common/nrf_sdh.h:
